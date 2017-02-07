c099f3cbc536e6ec25919c51b5c5e9218b4ee44e
Nimesh Ghelani <nimeshghelani@gmail.com>
2017-02-07 17:18:43 -0500

    working spa

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
+function hide_all(){
+    $('.container-fluid .row').addClass("hidden");
+}
+function render_players(){
+    hide_all();
+    $('#players').removeClass("hidden");
+}
+function render_heroes(){
+    hide_all();
+    $('#heroes').removeClass("hidden");
+}
+function render_recommend(id){
+    hide_all();
+    $('#recommend').removeClass("hidden");
+}
+
 var routes = {
+    '/': render_recommend,
     '/players': render_players,
     '/heroes': render_heroes,
     '/recommend': render_recommend,
-    '/recommend/:id': render_recommend
+    '/recommend/.*': render_recommend
 }
+initialize_router(routes)
+router.render()
--- js/kewlrouter.js
+++ js/kewlrouter.js
@@ -1,18 +1,67 @@
-var WILDCARD = 0;
-var VALUE = 0;
+var router;
+function parse_url(url, regex){
+    regex = regex || false;
+    var url_arr = url.split("/").filter(
+        function(x){return x.length > 0}
+    );
+    url_arr.push("");
+    if(regex){
+        url_arr = url_arr.map(function(x){return "^"+x+"$";})
+    }
+    return url_arr;
+}
+
 function parse_handler(handler){
     var parsed_handler = {}
     for(key in handler){
-        key = key.split("/").filter(function(x){return x.length > 0});
-        key.push(""):
+        var url_arr = parse_url(key, true);
         var cur_handler = parsed_handler;
-        for(var i = 0;i<key.length;i++){
-            if(key[i]=="*"){
-
+        for(var i = 0;i<url_arr.length;i++){
+            if(!cur_handler[url_arr[i]]){
+                if(i != url_arr.length - 1)
+                    cur_handler[url_arr[i]] = {}
+                else
+                    cur_handler[url_arr[i]] = handler[key];
             }
+            cur_handler = cur_handler[url_arr[i]];
         }
     }
+    return parsed_handler;
 }
+
 function KewlRouter(handler){
-    this.handler = handler;
+    this.root = '';
+    this.handler = parse_handler(handler);
+    this.render = function(){
+        var pathname = window.location.pathname;
+        var cur_handler = this.handler;
+        var url_arr = parse_url(pathname, false);
+        for(var i = 0; i < url_arr.length; i++){
+            var found = false;
+            for(var key in cur_handler){
+                if(new RegExp(key).test(url_arr[i])){
+                    cur_handler = cur_handler[key];
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) return false;
+        }
+        if(typeof(cur_handler) == "function"){
+            cur_handler(url_arr);
+            return true;
+        }else
+            return false;
+    }
+    this.navigate = function(e){
+        var target = e.target.href;
+        history.pushState(null, null, this.root + target);
+        this.render();
+        e.preventDefault();
+        return false;
+    }
+}
+
+function initialize_router(handler){
+    router = new KewlRouter(handler);
 }
