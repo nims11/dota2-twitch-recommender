9adfcab5c015d50ede4ab56db7c6c3274a275a67
Nimesh Ghelani <nimeshghelani@gmail.com>
2017-02-10 18:41:38 -0500

    Implement heroes and players page

--- js/app.js
+++ js/app.js
@@ -1,20 +1,6 @@
-function hide_all(){
-    $('.container-fluid .row').addClass("hidden");
-}
-function render_players(){
-    hide_all();
-    $('#players').removeClass("hidden");
-}
-function render_heroes(){
-    hide_all();
-    $('#heroes').removeClass("hidden");
-}
-function render_recommend(id){
-    hide_all();
-    $('#recommend').removeClass("hidden");
-}
-
 var data = null;
+var hero_to_player_popular = null;
+var player_to_hero_popular = null;
 function norm_vector(vec){
     var norm = Math.sqrt(vec.map(function(x){return x*x}).reduce(function(a, b){return a+b}, 0));
     return vec.map(function(x){return x / norm});
@@ -24,10 +10,11 @@ function populate_player_space(){
     var players = data['players'];
     var players_obj = {};
     for(var i = 0; i < players.length; i++){
-        players[i].vector = norm_vector(players[i].vector);
+        players[i].vector_norm = norm_vector(players[i].vector);
         players_obj[players[i].id] = players[i];
     }
     data['players'] = players_obj;
+    console.log("populated player space");
 }
 
 function get_hero_vector(player_id){
@@ -42,7 +29,6 @@ function get_hero_vector(player_id){
             for(var i = 0; i < data.length; i++){
                 vector[data[i]['hero_id']] += 1;
             }
-            vector = norm_vector(vector);
         }
     });
     return vector;
@@ -64,7 +50,7 @@ function kneighbors(pid, k){
         scores.push([
             pid,
             vector.map(
-                function(x, idx){return x*data['players'][pid].vector[idx]}).reduce(function(x, y){return x+y}
+                function(x, idx){return x*data['players'][pid].vector_norm[idx]}).reduce(function(x, y){return x+y}
                     , 0)
         ]
         );
@@ -80,18 +66,115 @@ function kneighbors(pid, k){
     return kneigh;
 }
 
-$.getJSON("/data.json", function(json){
-    console.log("Data Fetched...")
-    data = json;
-    populate_player_space();
-});
+function linkify_twitch(x){
+    return '<a href="'+x+'">'+x+"</a>";
+}
+
+function linkify_player(pid){
+    var url = "players/"+pid;
+    return '<a href="'+url+'" onclick="router.navigate(event)">'+data['players'][pid].name+"</a>";
+}
+
+function linkify_hero(hid){
+    var url = "heroes/"+hid;
+    return '<a href="'+url+'" onclick="router.navigate(event)">'+data['heroes'][hid]+"</a>";
+}
+
+function populate_populars(){
+    // popular heroes for a player
+    player_to_hero_popular = {};
+    for(var pid in data['players']){
+        var tmpArr = [];
+        for(var hid in data['players'][pid].vector){
+            if(!data['heroes'][hid])continue;
+            tmpArr.push([hid, data['players'][pid].vector[hid]]);
+        }
+        tmpArr.sort(function(x, y){return y[1] - x[1]});
+        player_to_hero_popular[pid] = tmpArr;
+    }
+
+    // render players page
+    var rows = []
+    for(var pid in player_to_hero_popular){
+        var summary = player_to_hero_popular[pid].slice(0, 5).map(
+            function(x){return linkify_hero(x[0]) + " (x"+x[1]+")";}
+        ).join(", ");
+        var name = data['players'][pid].name;
+        var twitch = linkify_twitch(data['players'][pid].twitch);
+        rows.push([name, "<tr><td>"+linkify_player(pid)+"</td><td>"+twitch+"</td><td>"+summary+"</td></tr>"])
+    }
+    rows.sort(function(x, y){return x[0].localeCompare(y[0]);});
+    rows = rows.map(function(x){return x[1]});
+    $('#players-table').append(rows.join(""));
+
+    // popular players for a hero
+    hero_to_player_popular = {};
+    for(var hid in data['heroes']){
+        if(!data['heroes'][hid])continue;
+        var tmpArr = [];
+        for(var pid in data['players']){
+            tmpArr.push([pid, data['players'][pid].vector[hid]]);
+        }
+        tmpArr.sort(function(x, y){return y[1] - x[1]});
+        hero_to_player_popular[hid] = tmpArr;
+    }
+
+    // render players page
+    rows = []
+    for(var hid in hero_to_player_popular){
+        var summary = hero_to_player_popular[hid].slice(0, 5).map(
+            function(x){return linkify_player(x[0]) + " (x"+x[1]+")";}
+        ).join(", ");
+        var name = data['heroes'][hid];
+        rows.push([name, "<tr><td>"+linkify_hero(hid)+"</td><td>"+summary+"</td></tr>"])
+    }
+    rows.sort(function(x, y){return x[0].localeCompare(y[0]);});
+    rows = rows.map(function(x){return x[1]});
+    $('#heroes-table').append(rows.join(""));
+}
+
+function hide_all(){
+    $('.container-fluid .row').addClass("hidden");
+}
+function render_players(){
+    hide_all();
+    $('#players').removeClass("hidden");
+}
+function render_heroes(){
+    hide_all();
+    $('#heroes').removeClass("hidden");
+}
+function render_recommend(id){
+    hide_all();
+    $('#recommend').removeClass("hidden");
+}
+function render_hero(id){
+
+}
+function render_player(id){
+
+}
 
 var routes = {
     '/': render_recommend,
     '/players': render_players,
     '/heroes': render_heroes,
+    '/players/.*': render_player,
+    '/heroes/.*': render_hero,
     '/recommend': render_recommend,
     '/recommend/.*': render_recommend
 }
-initialize_router(routes)
-router.render()
+$.ajax({
+    url: '/data.json',
+    dataType: 'json',
+    async: false,
+    success: function(json){
+        console.log("Data Fetched...")
+        data = json;
+        populate_player_space();
+        populate_populars();
+        initialize_router(routes)
+        router.render()
+    }
+});
+
